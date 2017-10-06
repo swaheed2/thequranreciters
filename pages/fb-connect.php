fb5761b95e0d7e58f23c67148b4da097171a7ad1
Sumama Waheed <sumamawaheed1@gmail.com>
2017-10-05 20:21:17 -0500

    clean v1

--- pages/fb-connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$path = $_SERVER['DOCUMENT_ROOT']; 
-	 
-  
-  
-?>
--- pages/gallery.php
+++
@@ -1,9 +0,0 @@
-	<?php 
-		echo '<div class="row reciter-container"<br>';
-		echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- Quran Reciters Pages Unit --><ins class="adsbygoogle"style="display:inline-block;width:100%;height:90px" data-ad-client="ca-pub-9126659759556489"
-				 data-ad-slot="5287989599"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script> ';
-		echo '</div> ';
-		echo '<div class="row reciter-container">';
-		echo getGallery($current); 
-		echo '</div>';
-	?> 
