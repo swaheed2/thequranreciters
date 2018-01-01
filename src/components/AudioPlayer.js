504f25492511e6b02d3b4150d4d257dde234a57c
Sumama Waheed <sumamawaheed1@gmail.com>
2017-12-31 18:55:18 -0600

    audio-player: use two rows

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -18,23 +18,49 @@ const styles = theme => {
 			width: '100%',
 			minHeight: '45px',
 			color: 'white',
+			padding: '10px 10px',
+			display: 'flex',
+			flexDirection: 'column'
+		},
+		playerBtns: {
+			display: 'flex',
+			flexDirection: 'row'
+		},
+		// player btns
+		playerBtn: {
+			flex: '1',
+			color: 'white',
+			flexBasis: 'auto',
+			width: '30px',
+			padding: '0px 10px',
+			width: '100%'
+		},
+		audioInfo:{
+			flex: '1',
 			display: 'flex',
 			flexDirection: 'row',
-			padding: '0px 10px'
+			width: '100%'
 		},
+		// audio info
 		title: {
 			color: 'white',
-			margin: '0px 15px'
+			margin: '0px 2%',
+			flexBasis: 'auto'
 		},
 		time: {
-			color: 'white'
-		},
-		playerBtn: {
-			color: 'white'
+			color: 'white',
+			flexBasis: 'auto',
+			marginRight: '20px'
 		},
 		slider: {
 			flex: '1',
-			margin: '0px 15px'
+			margin: '0px 0px',
+			paddingRight: '20px',
+			verticalAlign: 'bottom'
+		},
+		icon: {
+			width: '44px',
+			height: '44px'
 		}
 	}
 }
@@ -99,8 +125,10 @@ class AudioPlayer extends Component {
 		this.props.seekTo(progress);
 	}
 
-	formattedProgress() {
-		const p = this.props.progress;
+	formattedProgress(p) {
+		if (!p) {
+			p = 0;
+		}
 		const date = new Date(null);
 		date.setSeconds(p);
 		if (p < 3600) {
@@ -118,7 +146,11 @@ class AudioPlayer extends Component {
 
 		if (playing) {
 			playPauseBtn = (
-				<IconButton onClick={this.pause}
+				<IconButton
+					classes={{
+						icon: cls.icon
+					}}
+					onClick={this.pause}
 					className={cls.playerBtn} aria-label="Pause">
 					<Pause />
 				</IconButton>
@@ -126,7 +158,11 @@ class AudioPlayer extends Component {
 		}
 		else {
 			playPauseBtn = (
-				<IconButton onClick={this.play}
+				<IconButton
+					classes={{
+						icon: cls.icon
+					}}
+					onClick={this.play}
 					className={cls.playerBtn} aria-label="Play">
 					<PlayArrow />
 				</IconButton>
@@ -137,28 +173,53 @@ class AudioPlayer extends Component {
 
 			<Toolbar className={cls.audioPlayer}>
 
-				{playPauseBtn}
+				<div className={cls.playerBtns}>
 
-				<Typography type="title" className={cls.title}>
-					An-Nahl
-		        </Typography>
 
-				<Typography type="subheading" className={cls.time}>
-					{this.start()}
-				</Typography>
+					<IconButton
+						classes={{
+							icon: cls.icon
+						}}
+						onClick={this.play}
+						className={cls.playerBtn} aria-label="Play">
+						<PlayArrow />
+					</IconButton>
 
-				<div className={cls.slider}>
-					<Slider
-						onAfterChange={this.onAfterChange}
-						onBeforeChange={this.onBeforeChange}
-						step={.00001}
-						onChange={this.setProgress.bind(this)}
-						value={this.getProgress()} />
+					{playPauseBtn}
+
+					<IconButton
+						classes={{
+							icon: cls.icon
+						}}
+						onClick={this.play}
+						className={cls.playerBtn} aria-label="Play">
+						<PlayArrow />
+					</IconButton>
 				</div>
 
-				<Typography type="subheading" className={cls.time}>
-					{this.formattedProgress()}
-				</Typography>
+				<div className={cls.audioInfo}>
+					<Typography type="title" className={cls.title}>
+						An-Nahl
+		        	</Typography>
+
+					<Typography type="subheading" className={cls.time}>
+						{this.formattedProgress(this.props.progress)}
+					</Typography>
+
+					<div className={cls.slider}>
+						<Slider
+							style={{padding:'10px 0'}}
+							onAfterChange={this.onAfterChange}
+							onBeforeChange={this.onBeforeChange}
+							step={.00001}
+							onChange={this.setProgress.bind(this)}
+							value={this.getProgress()} />
+					</div>
+
+					<Typography type="subheading" className={cls.time}>
+						{this.formattedProgress(this.props.duration)}
+					</Typography>
+				</div>
 
 			</Toolbar>
 		)
